8228e590b854ba136c66b3f53640410c9d42f7b4
ayyush08 <ayushkumargupta2908@gmail.com>
2024-07-02 16:29:25 +0530

    feat: Add form submission functionality to SignUp component

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,24 +1,38 @@
 import React from 'react'
+import { useState,useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import EntryPage from './EntryPage'
 const SignUp = () => {
 
+    const [user, setUser] = useState({username:'', email:'', password:''})
+    useEffect(() => {
+        
+    }, [])
+    
     const navigate = useNavigate()
-    const handleSubmit = ()=>{
+    const handleSubmit = (e)=>{
+        e.preventDefault();
         console.log('signed in');
-        navigate('/entry-page')
+        // handleChange()
+        console.log(user);
+        // navigate('/entry-page')
+    }
+    const handleChange = (e)=>{
+        setUser({...user, [e.target.name]: e.target.value})
     }
     return (
     <div className='text-white flex justify-center items-center mt-16'>
         <div className='flex gap-5 flex-col border-white border-2 justify-center items-center md:h-[50vh] h-[70vh] w-[40vw] rounded-3xl '>  
             <h1 className='text-white text-4xl'>SignUp</h1>
             <div className="form flex flex-col transition-all">
+            <form onSubmit={handleSubmit}>
 
-            <input type='text' placeholder='Username' className='p-2 m-2 border-2 border-white rounded-lg text-black'/>
-            <input type='email' placeholder='Email' className='p-2 m-2 border-2 border-white rounded-lg text-black'/>
-            <input type='password' placeholder='Password' className='p-2 m-2 border-2 border-white rounded-lg text-black'/>
+            <input type='text' name='username' placeholder='Username' className='p-2 m-2 border-2 border-white rounded-lg text-black' value={user.username} onChange={handleChange}/>
+            <input type='email' name='email' placeholder='Email' className='p-2 m-2 border-2 border-white rounded-lg text-black' value={user.email} onChange={handleChange}/>
+            <input type='password' name='password' placeholder='Password' className='p-2 m-2 border-2 border-white rounded-lg text-black' value={user.password} onChange={handleChange}/>
+            <button className='p-2 m-2 border-2 border-white rounded-lg hover:bg-white hover:text-black transition-all ' type='submit'>SignUp</button>
+            </form>
             </div>
-            <button onClick={()=>handleSubmit()} className='p-2 m-2 border-2 border-white rounded-lg hover:bg-white hover:text-black transition-all'>SignUp</button>
         </div>
     </div>
     )
