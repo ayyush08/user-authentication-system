35b12fa5ff6f0155a72b806edd0fab6880a43920
AYUSH KUMAR GUPTA <ayushkumargupta2908@gmail.com>
2024-08-18 16:42:48 +0530

    feat: Add Blogs model for storing blog data

---
+++ backend/src/models/blogs.model.js
@@ -0,0 +1,24 @@
+import mongoose, { Schema } from "mongoose";
+
+
+const blogsSchema = new Schema({
+    owner:{
+        type:Schema.Types.ObjectId,
+        ref:'User',
+        required:true
+    },
+    title:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    content:{
+        type:String,
+        required:true,
+        trim:true
+    }
+},{
+    timestamps:true
+})
+
+export const Blogs = mongoose.model('Blogs',blogsSchema)
