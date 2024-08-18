e517711c2e54a8f325e2b6dc4ac33753665b966a
AYUSH KUMAR GUPTA <ayushkumargupta2908@gmail.com>
2024-08-18 16:45:26 +0530

    refactos

--- backend/src/models/blogs.model.js
+++ backend/src/models/blogs.model.js
@@ -10,12 +10,10 @@ const blogsSchema = new Schema({
     title:{
         type:String,
         required:true,
-        trim:true
     },
     content:{
         type:String,
         required:true,
-        trim:true
     }
 },{
     timestamps:true
